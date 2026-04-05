765d3c580e01e9f12870620ad6b47f421edd0452
MatthewKim323 <matthewykim23@gmail.com>
2026-04-05 03:44:16 -0700

    vercel dependencies to deploy

---
+++ api/index.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "backend"))
+os.chdir(os.path.join(os.path.dirname(__file__), "..", "backend"))
+
+from dotenv import load_dotenv
+load_dotenv(os.path.join(os.path.dirname(__file__), "..", "backend", ".env"))
+
+from main import app
